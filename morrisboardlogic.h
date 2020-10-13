55207bb7f102fde8639dc674b791a76c6212d564
CheetoDustNeo <misterrolypoly@gmail.com>
2020-10-12 21:31:30 -0500

    Missed a file. Still preparing for ASCII integration

--- morrisboardlogic.h
+++ morrisboardlogic.h
@@ -1,11 +1,17 @@
 #ifndef MORRISBOARDLOGIC_H
 #define MORRISBOARDLOGIC_H
+#include <set>
+#include <map>
 
+using std::set;
+using std::map;
 
 class MorrisBoardLogic
 {
 public:
     MorrisBoardLogic();
+private:
+
 };
 
 #endif // MORRISBOARDLOGIC_H
